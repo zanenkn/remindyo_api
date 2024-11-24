885d2acf7e3d66d8e78837665883356286f915ba
zanenkn <zane@queenslab.se>
2024-11-24 13:21:54 +0100

    adjust server.js to use auth routes and http-only cookie logic for login

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./db');
 const Birthday = require('./models/Birthday');
+const authRoutes = require('./routes/auth');
+const protect = require('./middleware/protect');
 
 const app = express();
 const PORT = process.env.PORT || 1337;
@@ -9,17 +12,24 @@ const PORT = process.env.PORT || 1337;
 require('dotenv').config();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
+    credentials: true,
   })
 );
 
 connectDB();
 
-app.post('/api/birthdays', async (req, res) => {
+app.use('/api/auth', authRoutes);
+
+app.post('/api/birthdays', protect, async (req, res) => {
   try {
-    const newBirthday = new Birthday(req.body);
+    const newBirthday = new Birthday({
+      ...req.body,
+      user: req.user,
+    });
     await newBirthday.save();
     res.status(201).json(newBirthday);
   } catch (err) {
@@ -27,9 +37,9 @@ app.post('/api/birthdays', async (req, res) => {
   }
 });
 
-app.get('/api/birthdays', async (req, res) => {
+app.get('/api/birthdays', protect, async (req, res) => {
   try {
-    const birthdays = await Birthday.find();
+    const birthdays = await Birthday.find({ user: req.user });
     res.status(200).json(birthdays);
   } catch (err) {
     res.status(500).json({ error: err.message });
